01395e690d5aa4d46cd6e4cd5fc46b401ff4ba37
Titus <letxtus@gmail.com>
2024-01-23 16:29:42 +0100

    init

---
+++ src/main/java/com/notizplaner/dev/NotizplanerApplication.java
@@ -0,0 +1,13 @@
+package com.notizplaner.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotizplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotizplanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notizplaner/dev/NotizplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notizplaner.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotizplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
