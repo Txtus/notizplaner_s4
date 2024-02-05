cafc9569da965acfd17ac5c5e8b2babfbf1279a1
QJS17 <157679418+QJS17@users.noreply.github.com>
2024-02-05 22:25:24 +0100

    Update src/main/java/com/notizplaner/dev/managers/db_manager.java

--- src/main/java/com/notizplaner/dev/managers/db_manager.java
+++ src/main/java/com/notizplaner/dev/managers/db_manager.java
@@ -27,7 +27,7 @@ public class db_manager {
 
         try (Statement stmt = connection_manager.createStatement(sqlName);
             Result resName = stmt.executeQuery(sqlName);
-            Result resText = stmt.executeQuery()
+     .executeQuery(sqlText)       Result resText = stmt.executeQuery()
         return notes;
     }
     public void updateNotes(ArrayList<Notiz> notes) {
