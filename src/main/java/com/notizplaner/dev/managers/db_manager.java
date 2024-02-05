a53646da5bc808aa752979a7c36e5d9d88d17dae
QJS17 <157679418+QJS17@users.noreply.github.com>
2024-02-05 22:27:47 +0100

    Update src/main/java/com/notizplaner/dev/managers/db_manager.java

--- src/main/java/com/notizplaner/dev/managers/db_manager.java
+++ src/main/java/com/notizplaner/dev/managers/db_manager.java
@@ -25,7 +25,9 @@ public class db_manager {
         String sql = "SELECT id, name, text FROM notes";
         try (Statement stmt = connection_manager.createStatement();
             ResultSet results = stmt.executeQuery(sql);) {
-                while (rs.next())
+                while (rs.next()) {
+                    
+                }
             }
         return notes;
     }
