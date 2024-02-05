c168c56385a63041034308d111dec90d1b21f112
QJS17 <157679418+QJS17@users.noreply.github.com>
2024-02-05 22:22:00 +0100

    Update src/main/java/com/notizplaner/dev/managers/db_manager.java

--- src/main/java/com/notizplaner/dev/managers/db_manager.java
+++ src/main/java/com/notizplaner/dev/managers/db_manager.java
@@ -25,7 +25,7 @@ public class db_manager {
         String sqlName = "SELECT name FROM notes";
         String sqlText = "SELECT text FROM notes";
 
-        try try (St)ate
+        try try (St)atement s
         return notes;
     }
     public void updateNotes(ArrayList<Notiz> notes) {
